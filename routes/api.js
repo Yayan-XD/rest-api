6b51223e6f707a5c329e26882a9856ab30bba580
Moch Yayan Juan Alvredo XD <69158355+Yayan-XD@users.noreply.github.com>
2021-07-11 03:38:13 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -8,7 +8,7 @@ var zahirr = db.get("zahirr");
 	console.log('')  
 }
  
-var creator = "@Tegar Dev"
+var creator = "@Yayan XD"
 var secure = require('ssl-express-www');
 var cors = require('cors');
 var fetch = require('node-fetch');
@@ -175,7 +175,7 @@ Akhir Pesan Error
 
 router.use(favicon(__path + "/views/favicon.ico"));
 
-const listkey = ["tegardev"];
+const listkey = ["yayanxd"];
 
 router.post("/apikey", async (req, res, next) => {
   const key = req.query.key;
